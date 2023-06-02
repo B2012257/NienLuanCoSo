ae1d9165f61dbbdd2012643f32135f4e22a1e9ae
ThaiPham <thaib2012257@student.ctu.edu.vn>
2023-06-02 13:17:36 +0700

    Update LogoutResponse.java

--- employerManagement/src/main/java/company/app/employermanagement/responses/LogoutResponse.java
+++ employerManagement/src/main/java/company/app/employermanagement/responses/LogoutResponse.java
@@ -2,31 +2,8 @@ package company.app.employermanagement.responses;
 
 import org.springframework.http.HttpStatus;
 
-public class LogoutResponse {
-    HttpStatus status;
-    String message;
-
-    public LogoutResponse() {
-    }
-
+public class LogoutResponse extends Response {
     public LogoutResponse(HttpStatus status, String message) {
-        this.status = status;
-        this.message = message;
-    }
-
-    public HttpStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(HttpStatus status) {
-        this.status = status;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
+        super(status, message);
     }
 }
