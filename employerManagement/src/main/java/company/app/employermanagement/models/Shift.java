56b0f1ff987fd18f29240d1ad0a72ed1ae651902
ThaiPham <thaib2012257@student.ctu.edu.vn>
2023-06-03 01:16:57 +0700

    Update Shift.java

--- employerManagement/src/main/java/company/app/employermanagement/models/Shift.java
+++ employerManagement/src/main/java/company/app/employermanagement/models/Shift.java
@@ -8,17 +8,15 @@ import java.util.Set;
 public class Shift {
     @Id
     private Long id;
-    private String name;
-    @Column(name = "task_id")
-    private String taskId;
+    @OneToOne
+    @JoinColumn(name = "task_id")
+    private Task taskId;
     @ManyToMany(mappedBy = "shifts")
     private Set<User> users;
     @ManyToOne
     @JoinColumn(name = "shiftListId", referencedColumnName = "id")
     private ShiftList shiftList;
-    @OneToOne
-    @JoinColumn(name = "task_id", referencedColumnName = "id", insertable = false, updatable = false)
-    private Task task;
+
 
     public Long getId() {
         return id;
@@ -28,19 +26,11 @@ public class Shift {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getTaskId() {
+    public Task getTaskId() {
         return taskId;
     }
 
-    public void setTaskId(String taskId) {
+    public void setTaskId(Task taskId) {
         this.taskId = taskId;
     }
 
@@ -59,13 +49,5 @@ public class Shift {
     public void setShiftListId(ShiftList shiftListId) {
         this.shiftList = shiftListId;
     }
-
-    public Task getTask() {
-        return task;
-    }
-
-    public void setTask(Task task) {
-        this.task = task;
-    }
-// Other work attributes, getters, setters, and relationships
+    
 }
